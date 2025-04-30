cd3bd5fad386199cb11c215052eac8d2245354a9
nabilcook87 <nabil@omega-solutions.co.uk>
2025-04-30 05:37:25 +0100

    Update pipe_sizing.py
    dfy54

--- utils/pipe_sizing.py
+++ utils/pipe_sizing.py
@@ -1,76 +1,68 @@
+# utils/pipe_sizing.py
+
 from utils.refrigerant_properties import RefrigerantProperties
-from utils.pressure_temp_converter import PressureTemperatureConverter
+from utils.friction_calculations import get_equivalent_length
 from utils.system_pressure_checker import check_pipe_rating
 from utils.oil_return_checker import check_oil_velocity
-from utils.pipe_length_volume_calc import PipeLengthVolumeCalculator
-from utils.friction_calculations import get_equivalent_length
+from utils.pipe_length_volume_calc import calculate_pipe_volume_liters
 import math
 
 class PipeSizer:
     def __init__(self):
         self.refrigerant_props = RefrigerantProperties()
-        self.temp_converter = PressureTemperatureConverter()
-        self.volume_calc = PipeLengthVolumeCalculator()
+        self.pipe_table = self.load_pipe_table()
 
-    def size_pipe(self, refrigerant, pipe_type, T_evap, T_cond, superheat_K, subcool_K,
-                  straight_length_m, capacity_kw, fixed_pipe_size=None,
-                  vertical_rise_m=0.0, fittings_equivalent_length_m=0.0):
+    def load_pipe_table(self):
+        import pandas as pd
+        df = pd.read_csv("data/pipe_pressure_ratings_full.csv")
+        return df.to_dict(orient="records")
 
-        props_evap = self.refrigerant_props.get_properties(refrigerant, T_evap)
-        props_cond = self.refrigerant_props.get_properties(refrigerant, T_cond)
+    def size_pipe(self, refrigerant, pipe_type, T_evap, T_cond, superheat_K, subcooling_K,
+                  pipe_length_m, evap_capacity_kw, fixed_pipe_size, vertical_rise_m=0.0,
+                  fittings_equivalent_length_m=0.0):
 
-        h_vap_start = props_evap["h_vapor"]
-        h_vap_end = self.refrigerant_props.get_properties(refrigerant, T_evap + 10)["h_vapor"]
-        Cp = (h_vap_end - h_vap_start) / 10
-        h_total = h_vap_start + Cp * superheat_K - props_cond["h_liquid"] + subcool_K * props_cond["Cp_liquid"]
+        props = self.refrigerant_props.get_properties(refrigerant, T_evap)
+        h_vap = props["enthalpy_vapor"]
+        h_vap_plus = self.refrigerant_props.get_properties(refrigerant, T_evap + 10)["enthalpy_vapor"]
+        Cp_vapor = (h_vap_plus - h_vap) / 10
+        h_subcooled = self.refrigerant_props.get_properties(refrigerant, T_cond)["enthalpy_liquid"]
+        Δh_kj_per_kg = Cp_vapor * superheat_K if pipe_type in ["Dry Suction", "Discharge"] else (
+            h_subcooled - self.refrigerant_props.get_properties(refrigerant, T_cond - subcooling_K)["enthalpy_liquid"]
+        )
+        m_dot_kg_s = (evap_capacity_kw) / Δh_kj_per_kg if Δh_kj_per_kg > 0 else 0.01
 
-        mass_flow_kg_s = (capacity_kw * 1000) / h_total
+        candidates = self.pipe_table if not fixed_pipe_size else [
+            pipe for pipe in self.pipe_table if pipe["Nominal Size (inch)"] == fixed_pipe_size
+        ]
 
-        pipe_table = self.refrigerant_props.get_pipe_table()
-
-        if fixed_pipe_size:
-            pipe_options = [p for p in pipe_table if p["Nominal Size (inch)"] == fixed_pipe_size]
-        else:
-            pipe_options = pipe_table
-
-        best_option = None
-
-        for pipe in pipe_options:
-            ID_m = pipe["ID_mm"] / 1000
+        best_pipe = None
+        for pipe in candidates:
+            ID_mm = pipe["ID_mm"]
+            ID_m = ID_mm / 1000.0
             area_m2 = math.pi * (ID_m / 2) ** 2
+            velocity = m_dot_kg_s / (area_m2 * props["density_vapor" if pipe_type in ["Dry Suction", "Discharge"] else "density_liquid"])
 
-            if pipe_type in ["Liquid", "Condenser Drain", "Pumped Refrigerant Liquid"]:
-                density = props_cond["density_liquid"]
-                viscosity = props_cond["viscosity_liquid"]
-            else:
-                density = props_evap["density_vapor"]
-                viscosity = props_evap["viscosity_vapor"]
+            total_equiv_length = pipe_length_m + fittings_equivalent_length_m
+            pressure_drop_fric = (
+                pipe["Friction Factor"] * (total_equiv_length / ID_m) * (props["density_vapor"] * velocity ** 2 / 2)
+            ) / 1000  # kPa
 
-            velocity_m_s = mass_flow_kg_s / (density * area_m2)
+            pressure_drop_lift = 9.81 * vertical_rise_m * props["density_vapor"] / 1000 if pipe_type in ["Dry Suction", "Discharge"] else 0
+            pressure_drop_total = pressure_drop_fric + pressure_drop_lift
 
-            total_length_m = straight_length_m + fittings_equivalent_length_m
-            friction_factor = 0.02
-            pressure_drop_friction = (friction_factor * total_length_m * density * velocity_m_s ** 2) / (2 * ID_m)
-            pressure_drop_vertical = density * 9.81 * vertical_rise_m
-            pressure_drop_total_pa = pressure_drop_friction + pressure_drop_vertical
-            pressure_drop_total_kpa = pressure_drop_total_pa / 1000
+            passes_velocity = check_oil_velocity(pipe_type, velocity, vertical_rise_m > 0)
+            rating_ok, _ = check_pipe_rating(pipe["Nominal Size (inch)"], T_cond, pressure_drop_total)
 
-            pressure_rating_ok = check_pipe_rating(pipe["Nominal Size (inch)"], pipe["Schedule"], T_cond, pressure_drop_total_kpa)
-            oil_ok = check_oil_velocity(pipe_type, velocity_m_s)
-
-            if pressure_rating_ok and oil_ok:
-                best_option = pipe
+            if passes_velocity and rating_ok:
+                best_pipe = {
+                    "selected_pipe": pipe,
+                    "velocity_m_s": velocity,
+                    "pressure_drop_total_kpa": pressure_drop_total,
+                    "mass_flow_kg_s": m_dot_kg_s
+                }
                 break
 
-        if not best_option:
-            raise ValueError("No suitable pipe found")
-
-        volume_liters = self.volume_calc.internal_volume_liters(best_option["ID_mm"], straight_length_m)
-
-        return {
-            "mass_flow_kg_s": mass_flow_kg_s,
-            "velocity_m_s": velocity_m_s,
-            "pressure_drop_total_kpa": pressure_drop_total_kpa,
-            "selected_pipe": best_option,
-            "internal_volume_liters": volume_liters
-        }
+        if best_pipe:
+            return best_pipe
+        else:
+            raise ValueError("No suitable pipe found for given conditions.")
