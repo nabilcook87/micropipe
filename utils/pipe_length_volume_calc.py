0a5472b4ad1410e43c2b0db873715708c0e5a76d
nabilcook87 <nabil@omega-solutions.co.uk>
2025-04-30 05:30:58 +0100

    Update pipe_length_volume_calc.py
    ngyr

--- utils/pipe_length_volume_calc.py
+++ utils/pipe_length_volume_calc.py
@@ -2,19 +2,19 @@
 
 import math
 
-class PipeLengthVolumeCalculator:
-    def __init__(self):
-        pass
-
-    def internal_volume_liters(self, diameter_mm, length_m):
-        """Calculate internal volume of pipe (liters) given diameter and length."""
-        diameter_m = diameter_mm / 1000
-        area_m2 = math.pi * (diameter_m / 2) ** 2
-        volume_m3 = area_m2 * length_m
-        return volume_m3 * 1000  # Convert to liters
-
-    def refrigerant_mass_kg(self, volume_liters, density_kg_m3):
-        """Calculate mass of refrigerant inside the volume."""
-        volume_m3 = volume_liters / 1000
-        mass_kg = volume_m3 * density_kg_m3
-        return mass_kg
+def calculate_pipe_volume_liters(diameter_mm, length_m):
+    """
+    Calculate internal volume of a cylindrical pipe in liters.
+    
+    Parameters:
+        diameter_mm (float): Internal diameter in millimeters
+        length_m (float): Length of the pipe in meters
+    
+    Returns:
+        float: Volume in liters
+    """
+    diameter_m = diameter_mm / 1000.0
+    radius_m = diameter_m / 2
+    area_m2 = math.pi * radius_m ** 2
+    volume_m3 = area_m2 * length_m
+    return volume_m3 * 1000  # Convert to liters
