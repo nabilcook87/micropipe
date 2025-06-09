00ad7484c40eaff63c0260cbd1781755a3f24d57
nabilcook87 <nabil@omega-solutions.co.uk>
2025-06-09 23:33:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ elif tool_selection == "Oil Return Velocity Checker":
     hdiff_10K = h_10K - h_evap
     hdiff_custom = hdiff_10K * min(max(superheat_K, 5), 30) / 10
     h_super = h_evap + hdiff_custom
-    h_foroil = (h_evap * 0.7) + (h_super * 0.3)
+    h_foroil = (h_evap * 0.705) + (h_super * 0.295)
     
     delta_h = h_evap - h_inlet
     delta_h_foroil = h_foroil - h_inlet
