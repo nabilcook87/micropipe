5aa668810558490561649981f9bbecc8f90adc31
nabilcook87 <nabil@omega-solutions.co.uk>
2025-06-09 10:10:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -200,8 +200,8 @@ elif tool_selection == "Oil Return Velocity Checker":
         density = (density_super + density_sat) / 2
         density_foroil = (density_super_foroil + density_sat) / 2
         velocity_m_s = adjusted_mass_flow_kg_s / (area_m2 * density)
-        oil_density_sat = (-0.886666666666667 * T_evap) + 961.577777777778
-        oil_density_super = (-0.886666666666667 * (T_evap + min(max(superheat_K, 5), 30))) + 961.577777777778
+        oil_density_sat = (0.00520509193776553 * (T_evap ** 2)) - (0.748839337715248 * T_evap) + 957.651634911391
+        oil_density_super = (0.00520509193776553 * (((T_evap + min(max(superheat_K, 5), 30))) ** 2)) - (0.748839337715248 * (T_evap + min(max(superheat_K, 5), 30))) + 957.651634911391
         oil_density = (oil_density_sat + oil_density_super) / 2
         MinMassFlux = (0.898 ** 2) * ((density_foroil * 9.81 * ID_m * (oil_density - density_foroil)) ** 0.5)
         MinMassFlow = MinMassFlux * area_m2
