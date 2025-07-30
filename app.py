1ec862a976b4186a79d4cb0134992fa319f4be5e
nabilcook87 <nabil@omega-solutions.co.uk>
2025-07-30 09:19:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ elif tool_selection == "Oil Return Checker":
         subcooling_K = st.number_input("Subcooling (K)", value=3.0)
     with col2:
         superheat_K = st.number_input("Superheat (K)", min_value=0.0, max_value=60.0, value=5.0)
-        max_penalty = st.number_input("Max Penalty (K)", min_value=0.1, max_value=6.0, value=1.0)
+        max_penalty = st.number_input("Max Penalty (K)", min_value=0.0, max_value=6.0, value=1.0)
         required_oil_duty_pct = st.number_input("Required Oil Return Duty (%)", min_value=0.0, max_value=100.0, value=100.0, step=5.0)
 
     from utils.refrigerant_properties import RefrigerantProperties
