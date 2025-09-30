8e5b5f33bebbe087a4a1eefdb3e7a5c9d5225fef
nabilcook87 <nabil@omega-solutions.co.uk>
2025-09-30 15:41:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1438,7 +1438,7 @@ elif tool_selection == "Manual Calculation":
 
             area_m2 = 3.1416 * (ID_m / 2) ** 2
 
-            density = RefrigerantProperties().get_properties(refrigerant, T_liq)["density_liquid"]
+            density = RefrigerantProperties().get_properties(refrigerant, T_liq)["density_liquid2"]
 
             velocity_m_s = mass_flow_kg_s / (area_m2 * density)
 
