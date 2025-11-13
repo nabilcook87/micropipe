b7c4fcfc694d2abea003bc0a0bb6030f08284afc
nabilcook87 <nabil@omega-solutions.co.uk>
2025-11-13 17:27:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3414,3 +3414,12 @@ elif tool_selection == "Manual Calculation":
         st.write(f"Fittings pressure loss: {dp_fittings_ws:.2f} kPa")
         st.write(f"Valve pressure loss: {dp_valves_ws:.2f} kPa")
         st.write(f"PLF loss: {dp_plf_ws:.2f} kPa")
+
+        st.write("d_vap:", d_vapour)
+        st.write("d_liq:", d_liquid)
+        st.write("h_vap:", h_out)
+        st.write("h_liq:", h_in)
+        st.write("deltah:", deltah)
+        st.write("base_massflow:", base_massflow)
+        st.write("Q_g:", Q_g)
+        st.write("velocity predicted:", gas_velocity)
