df9660accce3af9a932553901058308fde78ccab
nabilcook87 <nabil@omega-solutions.co.uk>
2025-12-14 19:15:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1037,8 +1037,14 @@ elif tool_selection == "Manual Calculation":
         with col4:
             ptrap = st.number_input("P Traps", min_value=0, max_value=10, value=0, step=1)
             ubend = st.number_input("U Bends", min_value=0, max_value=10, value=0, step=1)
-            ball = st.number_input("Ball Valves", min_value=0, max_value=20, value=0, step=1)
-            globe = st.number_input("Globe Valves", min_value=0, max_value=20, value=0, step=1)
+            if "double_trouble" in st.session_state:
+                ball = st.number_input("Ball Valves", min_value=0, max_value=20, value=0, step=1, disabled=True)
+            else:
+                ball = st.number_input("Ball Valves", min_value=0, max_value=20, value=0, step=1)
+            if "double_trouble" in st.session_state:
+                globe = st.number_input("Globe Valves", min_value=0, max_value=20, value=0, step=1, disabled=True)
+            else:
+                globe = st.number_input("Globe Valves", min_value=0, max_value=20, value=0, step=1)
             PLF = st.number_input("Pressure Loss Factors", min_value=0.0, max_value=20.0, value=0.0, step=0.1)
         
         col1, col2, col3, col4 = st.columns(4)
