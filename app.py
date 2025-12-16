9eb309dcc5d3fe8750b0e4106f682f376ddc90f6
nabilcook87 <nabil@omega-solutions.co.uk>
2025-12-16 14:05:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2226,8 +2226,6 @@ elif tool_selection == "Manual Calculation":
             )
         
         with col5:
-            if st.button("Double Riser") and not double_trouble:
-                st.warning("⚠️ Please enable **Double Riser Mode** before auto-selecting.")
             if st.button("Double Riser") and double_trouble:
                 small_candidates = []
                 for s in pipe_sizes:
