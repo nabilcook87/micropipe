edf2363142da4183f662f1741f7b1cfed7b41f6d
nabilcook87 <nabil@omega-solutions.co.uk>
2025-12-16 10:30:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1064,11 +1064,6 @@ elif tool_selection == "Manual Calculation":
         elif "selected_size" in ss and ss.selected_size in pipe_sizes:
             default_index = pipe_sizes.index(ss.selected_size)
 
-        is_double_riser = (
-            st.session_state.get("_auto_double_riser", False)
-            or st.session_state.get("double_trouble", False)
-        )
-
         disable_valves = st.session_state.get("double_trouble", False)
         
         with col1:
@@ -2232,7 +2227,6 @@ elif tool_selection == "Manual Calculation":
         
         with col5:
             if st.button("Double Riser"):
-                st.session_state["_auto_double_riser"] = True
                 results = []
                 failures = []
             
