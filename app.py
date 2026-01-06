8fc22fb7ff8c30ab336c8565c0ce991a11db1c80
nabilcook87 <nabil@omega-solutions.co.uk>
2026-01-06 14:30:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -77,6 +77,9 @@ def system_pressure_checker_ui():
             ["Suction", "Liquid", "Discharge", "Pumped"],
         )
 
+        if refrigerant == "R744 TC" and circuit == "Suction":
+            refrigerant = "R744"
+
         if circuit == "Discharge":
             mwp_options = 150
         else:
